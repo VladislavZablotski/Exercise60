d35414caf6c43faa5f15beadd3d927418f5f995b
unknown <zab_vl@tutby>
2023-04-06 23:57:32 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int[,,] array = new int[2,2,2];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        array[i,j,k]=new Random().Next(0,10);
+      }
+   }
+}
+Console.WriteLine("Массив: ");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+   for (int j = 0; j < array.GetLength(1); j++)
+   {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+         Console.Write(array[j,k,i]+$" ({j}, {k}, {i}) ");
+      }
+   Console.WriteLine();   
+   }
+}
